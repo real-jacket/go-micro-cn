91f4ad8f8d7cfe8ac0e7014a0f66576658431c54
jacket <1762982273@qq.com>
2020-09-10 16:14:37 +0800

    fix user-service sql

--- user-service/model/user/user_get.go
+++ user-service/model/user/user_get.go
@@ -8,7 +8,7 @@ import (
 
 func (s *service) QueryUserByName(userName string) (ret *proto.User, err error) {
 
-	queryString := `SELECT user_id, user_name, pwd  FROM user WHERE user_name = ?`
+	queryString := `SELECT user_id, user_name, pwd FROM user WHERE user_name = ?`
 
 	// 获取数据库
 	o := db.GetDB()
@@ -16,7 +16,7 @@ func (s *service) QueryUserByName(userName string) (ret *proto.User, err error) 
 	ret = &proto.User{}
 
 	// 查询
-	err = o.QueryRow(queryString, userName).Scan(ret.Id, ret.Name, ret.Pwd)
+	err = o.QueryRow(queryString, userName).Scan(&ret.Id, &ret.Name, &ret.Pwd)
 	if err != nil {
 		log.Logf("[QueryUserByName] 查询数据失败，err：%s", err)
 		return
